459fa8b73e0f32797261f02e4b9449c4a6521baa
rogwild <rogwild.design@gmail.com>
2023-10-31 02:03:38 +0700

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 const tdd = require('./tdd');
-const api = require('./api');
-const utils = require('./api');
-const providers = require('./providers');
-const plugins = require('./plugins');
-const middlewares = require('./middlewares');
 const seeder = require('./seeder');
 const dumper = require('./dumper');
 
 module.exports = {
-    utils,
     tdd,
-    api,
-    providers,
-    plugins,
-    middlewares,
     seeder,
     dumper,
 };
